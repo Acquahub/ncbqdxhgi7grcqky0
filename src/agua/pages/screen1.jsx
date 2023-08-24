855db88c81a73ddd68ec682e9edc8f84afe7c5db
david-dom <113909984+david-dom@users.noreply.github.com>
2023-08-24 09:44:37 -0500

    My commit message

---
+++ src/agua/pages/screen1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './screen1.module.css';
+
+
+
+function Screen1() {
+    
+
+    return(
+        <div className={styles["Screen1"]}>
+            
+<div id="text-1-8bbbf06122224a0ab4c22a0f959c3670" className={styles["text-1-8bbbf06122224a0ab4c22a0f959c3670"]} >Hola</div>
+        </div>
+    );
+}
+
+export default Screen1;
+            
